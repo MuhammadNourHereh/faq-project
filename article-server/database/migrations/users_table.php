2e3eb8e25348e94202106e48fec04a17bc92f62a
Muhammad Nour <acadamiclearner@gmail.com>
2025-03-08 14:24:18 +0200

    add users seeds

--- article-server/database/migrations/users_table.php
+++ article-server/database/migrations/users_table.php
@@ -6,7 +6,7 @@ mysqli_query($conn, "CREATE TABLE IF NOT EXISTS users (
     email varchar(255) PRIMARY KEY,
     first_name VARCHAR(225) NOT NULL,
     last_name VARCHAR(225) NOT NULL,
-    password_hash VARCHAR(225) NOT NULL
+    password VARCHAR(225) NOT NULL
 )");
 
 echo "users table was created successfully\n";
